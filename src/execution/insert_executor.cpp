c87cea4ddee9951565e7440c333bb133c9e55ae8
ubuntu <ubuntu@localhost.localdomain>
2022-08-17 21:00:13 +0800

    complete project3 update_executor

--- src/execution/insert_executor.cpp
+++ src/execution/insert_executor.cpp
@@ -41,25 +41,25 @@ auto InsertExecutor::Next([[maybe_unused]] Tuple *tuple, RID *rid) -> bool {
             Tuple tmp_tuple(values,&table_info_->schema_);
             // LOG_DEBUG("Insert a Tuple %s", tmp_tuple.ToString(&table_info_->schema_).c_str());
             if(!table_info_->table_->InsertTuple(tmp_tuple,rid,exec_ctx_->GetTransaction()))
-                throw Exception("Insert a Tuple faild:" + tmp_tuple.ToString(&table_info_->schema_));
+                throw Exception("Failed to Insert tuple:" + tmp_tuple.ToString(&table_info_->schema_));
 
             // update index 
             for(auto index:indexes_) {
-                auto meta = index->index_->GetMetadata();
-                auto key_tuple = tmp_tuple.KeyFromTuple(table_info_->schema_, index->key_schema_, meta->GetKeyAttrs());
-                index->index_->InsertEntry(key_tuple, *rid, exec_ctx_->GetTransaction()); 
+                auto keyAttrs = index->index_->GetKeyAttrs();
+                auto index_key = tmp_tuple.KeyFromTuple(table_info_->schema_, index->key_schema_, keyAttrs);
+                index->index_->InsertEntry(index_key, *rid, exec_ctx_->GetTransaction()); 
             }  
         }
     }else{
         while (child_executor_->Next(tuple, rid)) { 
             // LOG_DEBUG("Insert a Tuple %s", tuple->ToString(&table_info_->schema_).c_str());
             if(!table_info_->table_->InsertTuple(*tuple,rid,exec_ctx_->GetTransaction()))
-                throw Exception("Insert a Tuple faild:" + tuple->ToString(&table_info_->schema_));
+                throw Exception("Failed to Insert tuple:" + tuple->ToString(&table_info_->schema_));
             // update index 
             for(auto index:indexes_) {
-                auto meta = index->index_->GetMetadata();
-                auto key_tuple = tuple->KeyFromTuple(table_info_->schema_, index->key_schema_, meta->GetKeyAttrs());
-                index->index_->InsertEntry(key_tuple, *rid, exec_ctx_->GetTransaction()); 
+                auto keyAttrs = index->index_->GetKeyAttrs();
+                auto index_key = tuple->KeyFromTuple(table_info_->schema_, index->key_schema_, keyAttrs);
+                index->index_->InsertEntry(index_key, *rid, exec_ctx_->GetTransaction()); 
             }  
         }
     }
--- src/execution/update_executor.cpp
+++ src/execution/update_executor.cpp
@@ -12,16 +12,45 @@
 #include <memory>
 
 #include "execution/executors/update_executor.h"
+#include "common/logger.h"
 
 namespace bustub {
 
 UpdateExecutor::UpdateExecutor(ExecutorContext *exec_ctx, const UpdatePlanNode *plan,
                                std::unique_ptr<AbstractExecutor> &&child_executor)
-    : AbstractExecutor(exec_ctx) {}
+    : AbstractExecutor(exec_ctx),
+    plan_(plan),
+    child_executor_(std::move(child_executor)){}
 
-void UpdateExecutor::Init() {}
+void UpdateExecutor::Init() {
+  auto catalog = exec_ctx_->GetCatalog();
+  table_info_ = catalog->GetTable(plan_->TableOid());
+  indexes_ = catalog->GetTableIndexes(table_info_->name_);
 
-auto UpdateExecutor::Next([[maybe_unused]] Tuple *tuple, RID *rid) -> bool { return false; }
+  if(child_executor_ != nullptr) 
+    child_executor_->Init();
+}
+
+auto UpdateExecutor::Next([[maybe_unused]] Tuple *tuple, RID *rid) -> bool { 
+  while(child_executor_->Next(tuple,rid)) {
+    // LOG_DEBUG("updale a Tuple %s", tuple->ToString(&table_info_->schema_).c_str());
+    auto tmp_tuple = GenerateUpdatedTuple(*tuple);
+
+    if(!table_info_->table_->UpdateTuple(tmp_tuple,*rid,exec_ctx_->GetTransaction()))
+        throw Exception("Failed to Update tuple:" + tmp_tuple.ToString(&table_info_->schema_));
+    // update index 
+    for(auto index:indexes_) {
+        auto keyAttrs = index->index_->GetKeyAttrs();
+        auto index_key = tuple->KeyFromTuple(table_info_->schema_, index->key_schema_, keyAttrs);
+        // del previous index_key
+        index->index_->DeleteEntry(index_key,*rid,exec_ctx_->GetTransaction());
+        // add new index_key
+        index_key = tmp_tuple.KeyFromTuple(table_info_->schema_, index->key_schema_, keyAttrs);
+        index->index_->InsertEntry(index_key, *rid, exec_ctx_->GetTransaction()); 
+    }
+  }
+  return false; 
+}
 
 auto UpdateExecutor::GenerateUpdatedTuple(const Tuple &src_tuple) -> Tuple {
   const auto &update_attrs = plan_->GetUpdateAttr();
